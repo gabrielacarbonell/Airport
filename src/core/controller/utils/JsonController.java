51971c2423c6acbc9369382c76bf74c1377a2576
Jefferson Torres <gabrielacarbonell@uninorte.edu.co>
2025-05-21 22:10:30 -0500

    Reorganizacion a MVC 2.0

--- src/core/controller/utils/JsonController.java
+++ src/core/controller/utils/JsonController.java
@@ -6,8 +6,46 @@ package core.controller.utils;
 
 /**
  *
- * @author JJTorres
+ * @author Gabyy
  */
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
 public class JsonController {
-    
+    public static ArrayList<ArrayList<Object>> readJson(String archivo){
+        ArrayList<ArrayList<Object>> jsonC = new ArrayList<>(); 
+        try {
+            String contenido = new String(Files.readAllBytes(Paths.get("./json/"+archivo+".json")));
+
+            // Convertir a JSONArray
+            JSONArray jsons = new JSONArray(contenido);
+            Map<String,Object> jsonP = jsons.getJSONObject(0).toMap();
+            Set<String> claves = jsonP.keySet();
+            
+            ArrayList<Object> valores = new ArrayList<>();
+            for (int i = 0; i < jsons.length(); i++) {
+                JSONObject json = jsons.getJSONObject(i);
+                for (String clave : claves) {
+                    try{
+                        valores.add(json.getString(clave));
+                    }catch(Exception e){
+                        valores.add(json.getInt(clave));
+                    }
+                }
+                jsonC.add(valores);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        } catch (org.json.JSONException e) {
+            System.out.println("Error al procesar el JSON: " + e.getMessage());
+        }
+        return jsonC;
+    }
 }
